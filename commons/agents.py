d242c593dd824b0b2e3f9b56d2fa5240df9bdd01
Denis Rothman <30811222+Denis2054@users.noreply.github.com>
2024-12-14 13:49:46 +0100

    Add files via upload

---
+++ commons/agents.py
@@ -0,0 +1,13 @@
+def web_search(response, user_content):
+   response="This is the result of a web search:"
+   return response
+
+def RAG(response, user_content):
+   response="This is an RAG function response:"
+   return response
+
+def ML_function(response, user_content):
+   response="This is an ML function response:"
+   return response
+
+
