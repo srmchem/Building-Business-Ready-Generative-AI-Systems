23daf1927043d3e24fb1e966e5676f39880f3ff4
Denis Rothman <30811222+Denis2054@users.noreply.github.com>
2024-12-17 18:52:10 +0100

    Update agents.py

--- commons/agents.py
+++ commons/agents.py
@@ -5,5 +5,7 @@ def RAG(response, user_content):
 def ML_function(response, user_content):
    response="This is an ML function response:"
    return response
-
+def Web_search(response, user_content):
+   response="This is a web search response:"
+   return response
 
