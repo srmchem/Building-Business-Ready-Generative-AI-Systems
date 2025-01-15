91dc26df28265a31fb577d65a25ecfcb1d17ec1e
Denis Rothman <30811222+Denis2054@users.noreply.github.com>
2025-01-15 14:01:09 +0100

    Add files via upload

---
+++ commons/machine_learning.py
@@ -0,0 +1,7 @@
+# Implemented in Chapter06
+def bayes(input, data):
+    #Endpoint for the Bayes algorithm that will be implemented in Chapter 6
+    if input=="":
+      # Testing the endpoint in a CoT (Chain of Reasoning)
+      text="The customers would like more activities during their trips especially on the ocean."
+    return text
