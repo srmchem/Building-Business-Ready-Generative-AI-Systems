1b6846e61ac3c02ae17f1ccd3a20347294d58de1
Denis Rothman <30811222+Denis2054@users.noreply.github.com>
2025-01-15 14:04:00 +0100

    Add files via upload

---
+++ commons/web_search.py
@@ -0,0 +1,7 @@
+# Implemented in Chapter06
+def search(query):
+    #Endpoint for the web search function that will be implemented in Chapter 6
+    if query=="":
+      # Testing the endpoint in a CoT (Chain of Reasoning)
+      text="Antartica Cruise and Conference holidays."
+    return text
