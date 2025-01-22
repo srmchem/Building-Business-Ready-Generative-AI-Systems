1cbbfbb6a0abdfababe60d122d9b9cb08a8f48d9
Denis Rothman <30811222+Denis2054@users.noreply.github.com>
2025-01-22 08:39:50 +0100

    Update machine_learning.py

--- commons/machine_learning.py
+++ commons/machine_learning.py
@@ -5,3 +5,11 @@ def bayes(input, data):
       # Testing the endpoint in a CoT (Chain of Reasoning)
       text="The customers would like more activities during their trips especially on the ocean."
     return text
+
+# Implemented in Chapter06
+def ml_agent(input, data):
+    #Endpoint for the Bayes algorithm that will be implemented in Chapter 6
+    if input=="":
+      # Testing the endpoint in a CoT (Chain of Reasoning)
+      text="The customers would like more activities during their trips especially on the ocean."
+    return text
