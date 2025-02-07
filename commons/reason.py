f908adb63f45e40e61f7e1bda197142679fc1746
Denis Rothman <30811222+Denis2054@users.noreply.github.com>
2025-02-07 20:41:22 +0100

    Update reason.py

--- commons/reason.py
+++ commons/reason.py
@@ -118,45 +118,7 @@ def generate_image(prompt, model="dall-e-3", size="1024x1024", quality="standard
 
     # Extract and return the image URL from the response
     return response.data[0].url
-
-#Implemented in Chapter07
-
-import os
-
-# Define the cache directory in your Google Drive
-cache_dir = '/content/drive/MyDrive/genaisys/HuggingFaceCache'
-
-# Set environment variables to direct Hugging Face to use this cache directory
-os.environ['TRANSFORMERS_CACHE'] = cache_dir
-#os.environ['HF_DATASETS_CACHE'] = os.path.join(cache_dir, 'datasets')
-from transformers import AutoTokenizer, AutoModelForCausalLM
-# Initialize the tokenizer and model
-
-# Define the path to the model directory
-model_name = '/content/drive/MyDrive/genaisys/HuggingFaceCache/models--unsloth--DeepSeek-R1-Distill-Llama-8B/snapshots/71f34f954141d22ccdad72a2e3927dddf702c9de'
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name, device_map='auto', torch_dtype='auto')
-
-def deepseekr1distill(prompt,max_new_tokens,temperature,top_k,top_p,do_sample):
-  # Tokenize the input
-  inputs = tokenizer(prompt, return_tensors='pt').to('cuda')
-
-  # Generate output
-  outputs = model.generate(**inputs, max_new_tokens=500)
-  # Generate output with specified parameters
-  outputs = model.generate(
-    **inputs,
-    max_new_tokens=500,
-    temperature=0.7,  # Adjust as needed
-    top_k=50,        # Adjust as needed
-    top_p=0.9,       # Adjust as needed
-    do_sample=True   # Set to False for deterministic output
-  )
-
-  # Decode and display the output
-  generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-  return generated_text
-
+    
 # Import the function from custom machine learning file
 import os
 import machine_learning
