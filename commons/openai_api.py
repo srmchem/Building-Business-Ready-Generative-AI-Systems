681a82469778878ae0ef9cdee9205d3ec5d64148
Denis Rothman <30811222+Denis2054@users.noreply.github.com>
2025-07-10 11:11:44 +0200

    Update openai_api.py

--- commons/openai_api.py
+++ commons/openai_api.py
@@ -1,8 +1,6 @@
 from openai import OpenAI
 import openai
 from openai import OpenAI
-# Initialize the OpenAI client
-client = OpenAI()
 import base64
 
 # Implemented in Chapter01
@@ -82,7 +80,10 @@ def image_analysis(image_path_or_url, query_text, model="gpt-4o"):
         "frequency_penalty": 0,
         "presence_penalty": 0,
     }
-
+    
+    # Initialize the OpenAI client
+    client = OpenAI()
+    
     # Make the API call
     response = client.chat.completions.create(
         model=model,
